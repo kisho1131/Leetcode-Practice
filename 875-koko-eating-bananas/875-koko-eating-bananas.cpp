c95126ce0b3359f9ddd57add61a640ce84ab3706
Sumit Kumar <63787026+kisho1131@users.noreply.github.com>
2022-05-08 02:40:15 +0530

    Time: 59 ms (55.93%), Space: 19 MB (26.50%) - LeetHub

---
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPossible(vector<int>&arr, int speed, int maxH){
+            int currH= 0;
+        for(int i =0;i<arr.size();i++){
+            // (p + m - 1) / m equal to ceil(p / m) (just personal behavior)
+            currH += (arr[i] + speed - 1) / speed;
+        }
+        return currH <= maxH;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int st = 1;
+        int ed = *max_element(piles.begin(), piles.end());
+        int ans = INT_MAX;
+        if(h == piles.size()) return ed;
+        while(st <= ed){
+            int speed = st + (ed-st)/2;
+            if(isPossible(piles, speed, h ) == true){
+                ed = speed - 1;
+                ans = speed;
+            }
+            else
+                st = speed  + 1;
+        }
+        return ans;
+    }
+};
