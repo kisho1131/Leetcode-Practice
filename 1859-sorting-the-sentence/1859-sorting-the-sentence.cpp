3f57eecb49de57c672494b1c93e4790a94a2b6a0
Sumit Kumar <63787026+kisho1131@users.noreply.github.com>
2022-07-05 00:19:28 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (35.90%) - LeetHub

---
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string sortSentence(string s) {
+        stringstream words(s);
+        map<string, int>mp;
+        // vector<int>ans;
+        vector<pair<int, string>>temp;
+        
+        string word;
+        while(words >> word){
+            int len = word.size();
+            int i = int(word[len-1]) - 48;
+            temp.push_back(make_pair(i, word.substr(0, len-1)));
+        }
+        
+        sort(temp.begin(), temp.end());
+        
+        string ans = "";
+        int n = temp.size();
+        for(int i =0;i<n;i++){
+            ans += temp[i].second;
+            if(i!= n-1)
+                ans+= " ";
+        }
+        return ans;
+    }
+};
