a506bca0ca3760f0ed43d395b1293cc43e89ae79
Sumit Kumar <63787026+kisho1131@users.noreply.github.com>
2022-07-15 02:22:03 +0530

    Time: 6 ms (89.59%), Space: 6 MB (91.33%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void helper(string tiles, int &result, int begin) {
+        result++;
+        for(int i=begin;i<tiles.size();i++) {
+            if(i!=begin && tiles[i]==tiles[begin])  continue;
+            swap(tiles[i],tiles[begin]);
+            helper(tiles,result,begin+1);
+        }
+        return;
+    }
+    int numTilePossibilities(string tiles) {
+        sort(tiles.begin(),tiles.end());
+        int result=-1;
+        helper(tiles,result,0);
+
+        return result;
+    }
+};
