4ad19ff916b8d88c4fbeb786e4c692bd08f40f9a
Sumit Kumar <63787026+kisho1131@users.noreply.github.com>
2022-07-20 00:53:48 +0530

    Time: 36 ms (26.79%), Space: 7.4 MB (47.02%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool checkInclusion(string p, string s) {
+        int i =0, j =0;
+        // vector<int>res;
+        int k = p.length();
+        unordered_map<int, int>mp;
+        for(auto ele : p)
+            mp[ele]++;
+        int count = mp.size();
+        while(j < s.length()){
+            if(mp.find(s[j])!=mp.end()){
+                mp[s[j]]--;
+                if(mp[s[j]] == 0) count--;
+            }
+            if((j - i + 1) < k)
+                j++;
+            else if ( (j-i+1) == k){
+                if(count == 0) return true;
+                
+                if(mp.find(s[i]) != mp.end()){
+                    mp[s[i]]++;
+                    if(mp[s[i]] == 1) count++;
+                }
+                i++;
+                j++;
+            }
+        }
+        
+        return false;
+    }
+};
