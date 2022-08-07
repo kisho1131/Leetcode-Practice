97c326e47ccab1f687bca9369b7ce9ef838a72e4
Sumit Kumar <63787026+kisho1131@users.noreply.github.com>
2022-08-07 23:33:58 +0530

    Time: 9 ms (23.65%), Space: 7.8 MB (45.39%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     vector<vector<int>>res;
+    void helper(vector<int>&nums, vector<int>&ds, int freq[]){
+        if(ds.size() == nums.size()){
+            res.push_back(ds);
+            return;
+        }
+        
+        for(int i =0;i<nums.size();i++){
+            if(!freq[i]){
+                freq[i] = 1;
+                ds.push_back(nums[i]);
+                helper(nums, ds, freq);
+                freq[i] = 0;
+                ds.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int>ds;
+        int freq[nums.size()];
+        memset(freq, 0, sizeof(freq));
+        helper(nums, ds, freq);
+        return res;
+    }
+};
+/*
+    vector<vector<int>>res;
     void permute_rec(int st,int ed, vector<int>&nums){
         if(st >= ed){
             res.push_back(nums);
@@ -18,4 +44,4 @@ public:
         permute_rec(0,nums.size()-1,nums);
         return res;
     }
-};
+*/
