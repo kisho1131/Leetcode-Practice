256cfebd68a72efc85cfcdac82778cb4a1707745
Sumit Kumar <63787026+kisho1131@users.noreply.github.com>
2022-08-31 03:24:03 +0530

    Time: 729 ms (8.59%), Space: 130.5 MB (11.92%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -10,8 +10,9 @@ public:
             for(int i = 2;i<n;++i){
                 if(is_prime[i]){
                     count++;
-                    for(int j = 2*i; j<n;j+=i)
-                        is_prime[j] = 0;
+                    if((long long) i*i <= n)
+                        for(int j = i*i; j<n;j+=i)
+                            is_prime[j] = 0;
                 } 
             }
         return count++;
