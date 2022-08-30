a303ea3add8683f72ebfb8e3fed7d86ec7b2aa04
Sumit Kumar <63787026+kisho1131@users.noreply.github.com>
2022-08-31 03:16:40 +0530

    Time: 890 ms (5.05%), Space: 130.6 MB (7.36%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -4,7 +4,8 @@ public:
     int countPrimes(int n) {
         if(n < 2) return 0;
         int count = 0;
-        vector<int>is_prime(n, 1);
+        // n = sqrt(n);
+        vector<int>is_prime(n+1, 1);
         is_prime[0] = is_prime[1] = 0;
             for(int i = 2;i<n;++i){
                 if(is_prime[i]){
